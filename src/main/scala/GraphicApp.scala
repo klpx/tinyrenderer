918eabb399e920399eba117bfc539c3ba3d2efc3
Alexander Hasselbach <alex@arigativa.ru>
2015-02-01 00:37:09 +0600

    Finish Part 1. Fix canvas projection

--- src/main/scala/GraphicApp.scala
+++ src/main/scala/GraphicApp.scala
@@ -14,8 +14,8 @@ object GraphicApp extends App {
 }
 
 class Viewport extends JPanel {
-  private val _width = 320
-  private val _height = 240
+  private val _width = 400
+  private val _height = 400
   
 	override def getPreferredSize = new Dimension(_width, _height)
 
@@ -23,14 +23,17 @@ class Viewport extends JPanel {
 		super.paintComponent(g)
 		// g.drawPoint(new Point(50, 50), RED)
     
+    def normalizeX(x: Double): Int = ((x+1) * _width / 2).toInt
+    def normalizeY(y: Double): Int = _height - ((y+1) * _height / 2).toInt
+    
     val faces = WavefrontParser.parse(io.Source.fromFile("african_head.obj"))
     g.drawString(s"drawing ${faces.length} faces", 10, 20)
     for(face <- faces) {
       for (edge <- face.edges) {
-        val x0: Int = ((edge._1.x+1) * _width / 2).toInt
-        val y0: Int = ((edge._1.y+1) * _height / 2).toInt
-        val x1: Int = ((edge._2.x+1) * _width / 2).toInt
-        val y1: Int = ((edge._2.y+1) * _height / 2).toInt
+        val x0: Int = normalizeX(edge._1.x)
+        val y0: Int = normalizeY(edge._1.y)
+        val x1: Int = normalizeX(edge._2.x)
+        val y1: Int = normalizeY(edge._2.y)
         g.drawLine(x0, y0, x1, y1)
       }
     }
